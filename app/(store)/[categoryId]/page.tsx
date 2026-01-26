990f3e484ab60a3c8704e45553a9dcda485510e4
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-01-25 19:41:28 -0800

    dev(category) : zod fixed

--- app/(store)/[categoryId]/page.tsx
+++ app/(store)/[categoryId]/page.tsx
@@ -1,14 +1,18 @@
+import {CategoryWithProductsResponseSchema} from "@/app/src/schema";
+
 type Params = Promise<{categoryId: string}>;
 
 async function getProducts(categoryId: string) {
     const url = `${process.env.API_URL}/categories/${categoryId}?products=true`
     const req = await fetch(url)
     const json = await req.json()
-
-return json}
+    console.log(json)
+    return CategoryWithProductsResponseSchema.parse(json)
+}
 export default async function StorePage({params}: { params: Params}) {
     const {categoryId} = await params
     const products= await getProducts(categoryId)
+    console.log(products)
     return(
         <div>Page</div>
     )
---
+++ app/src/schema.ts
@@ -0,0 +1,21 @@
+import {z} from 'zod'
+
+export const ProductSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    image: z.string(),
+    price: z.coerce.number(),
+    stock: z.number(),
+    category: z.number().or(z.object({
+        id: z.number()
+    }))
+})
+
+export const CategorySchema = z.object({
+    id: z.number(),
+    name: z.string()
+})
+
+export const CategoryWithProductsResponseSchema = CategorySchema.extend({
+    products: z.array(ProductSchema)
+});
