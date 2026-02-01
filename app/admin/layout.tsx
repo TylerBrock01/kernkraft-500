0622977d3638cd580cb1163b5e5bb4b53c207a2a
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-01-31 17:33:23 -0800

    dev(admin) : page sales added

---
+++ app/admin/layout.tsx
@@ -0,0 +1,20 @@
+import ToastNotification from "@/components/UI/ToastNotification";
+import AdminNav from "@/components/UI/AdminNav";
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <AdminNav/>
+            <div className="lg:min-h-screen container mx-auto mt-10 px-10 lg:px-0">
+                <div className="bg-white shadow w-full  mx-auto p-10 my-10 lg:w-3/5" >
+                    {children}
+                </div>
+            </div>
+            <ToastNotification />
+        </>
+    );
+}
--- components/UI/MainNav.tsx
+++ components/UI/MainNav.tsx
@@ -25,6 +25,7 @@ export default async function MainNav() {
 
                         {category.name}
                     </Link>))}
+                <Link href={'/admin/sales'} className={'rounded bg-green-400 font-bold py-2 px-10 text-purple-600 capitalize'}>panel admin</Link>
             </nav>
         </header>
     )
