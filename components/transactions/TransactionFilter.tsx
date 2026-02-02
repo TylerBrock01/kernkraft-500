61f2dc70da77c6553526e3d848d245133f145160
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-01 16:46:24 -0800

    dev(admin) : added tanstack &formatted date

--- components/transactions/TransactionFilter.tsx
+++ components/transactions/TransactionFilter.tsx
@@ -4,16 +4,24 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"
 import {useState} from "react";
 import {format} from "date-fns";
+import {useQuery} from "@tanstack/react-query";
+import {getSalesByDate} from "@/src/api";
 
 type ValuePiece = Date |null
 type Value = ValuePiece | [ValuePiece, ValuePiece]
+
 export default function TransactionFilter() {
     const [date, setDate] = useState<Value>(new Date())
-    const formattedDate = format(date!.toString()!, "yyyy-MM-dd")
+    const formattedDate = format(date!.toString(), "yyyy-MM-dd")
+    const {data, isLoading} = useQuery({
+        queryKey:['sales',formattedDate],
+        queryFn: ()=> getSalesByDate(formattedDate)
+    })
+
     return (
         <div className={"grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10"}>
             <div>
-                <Calendar value={date} onChange={setDate}/>
+                <Calendar locale="es-ES" value={date} onChange={setDate}/>
             </div>
         </div>
     )
