16b365698ec3d45b159bce61f46d81a19707f6e9
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-02 17:06:43 -0800

    dev(products) : pagination added to url

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,17 +1,29 @@
 import Heading from "@/components/UI/Heading";
 import {ProductResponseSchema} from "@/src/schema";
 import ProductsTable from "@/components/products/ProductsTable";
-async function getProducts(){
-    const url = `${process.env.API_URL}/products`;
+import {redirect} from "next/navigation";
+import {isValidPage} from "@/src/utils";
+async function getProducts(take:number, skip:number){
+    const url = `${process.env.API_URL}/products?take=${take}&skip=${skip}`;
     const req = await fetch(url)
     const json = await req.json()
     const data = ProductResponseSchema.parse(json)
-
-    return {products: data.products, total: data.products.length};
+    return {products: data.products, total: data.total};
 }
 
-export default async function ProductsPage() {
-    const {products, total} =await getProducts()
+type SearchParams = Promise<{page:string}>
+
+export default async function ProductsPage({searchParams}: {searchParams: SearchParams}) {
+    const {page} = await searchParams
+    if (!isValidPage(+page)) redirect('/admin/products?page=1');
+
+    const producstPerPage = 10
+    const skip = (+page - 1) * producstPerPage;
+    const {products, total} =await getProducts(producstPerPage,skip)
+    const totalPages = Math.ceil( total/producstPerPage );
+    if (+page > totalPages) redirect('/admin/products?page=1');
+
+
     return (
         <>
             <Heading>Administrar Productos</Heading>
