2181dca129c43f1efda677ac465e36cf6ba623e1
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-04 12:34:32 -0800

    dev (uploadImage): dropzone added

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -1,4 +1,5 @@
 import {CategoriesResponseSchema, Product} from "@/src/schema";
+import UploadProductImage from "@/components/products/UploadProductImage";
 
 async function getCategories(){
     const url = `${process.env.API_URL}/categories`;
@@ -75,7 +76,7 @@ export default async function ProductForm({product} :{product?: Product}) {
                     ))}
                 </select>
             </div>
-
+            <UploadProductImage/>
         </>
     )
 }
