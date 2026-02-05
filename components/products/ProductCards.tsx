b1cf7d4bdec84bba047f722f413df0d12f8b761b
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-04 16:58:56 -0800

    dev (uploadImage): sold out added

--- components/products/ProductCards.tsx
+++ components/products/ProductCards.tsx
@@ -1,5 +1,5 @@
 import {Product} from "@/src/schema";
-import {formatCurrency, getImagePath} from "@/src/utils";
+import {formatCurrency, getImagePath, isAvalaible} from "@/src/utils";
 import Image from "next/image";
 import AddProductButton from "@/components/products/AddProductButton";
 
@@ -8,7 +8,7 @@ export default function ProductCard({product}: { product: Product}) {
         <div
             className='rounded bg-white shadow relative p-5'
         >
-            <div>
+            <div className={`${!isAvalaible(product.stock)&& 'opacity-40'}`}>
                 <Image
                     src={getImagePath(product.image)}
                     alt={`imagen de ${product.name}`}
@@ -23,7 +23,9 @@ export default function ProductCard({product}: { product: Product}) {
                     <p className="text-2xl font-extrabold  text-gray-900">{formatCurrency(product.price)}</p>
                 </div>
             </div>
-            <AddProductButton product={product}/>
+            {isAvalaible(product.stock) ? (
+                <AddProductButton product={product}/>
+            ):<p className={"absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1 bg-white opacity-60 w-full text-center py-5 text-2xl uppercase font-black"}>Agotado</p>}
         </div>
     )
 }
