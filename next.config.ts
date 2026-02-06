3f4eb8a3e4859a0bc21a7d77c69a412155ade038
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-05 16:25:00 -0800

    config added v2

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images:{
         remotePatterns: [
-            // {
-            //     protocol: 'https',
-            //     hostname: 'kernkraft-400.onrender.com'
-            // },
+            {
+                protocol: 'https',
+                hostname: 'kernkraft-400.onrender.com'
+            },
             {
                 protocol:'https',
                 hostname: 'res.cloudinary.com',
