9019ce952294ec6cfe087eb3b91b55edab9b24d1
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-08 12:28:28 -0800

    category navigation added

---
+++ components/UI/CategoryNav.tsx
@@ -0,0 +1,15 @@
+import {Category} from "@/src/schema";
+import Link from "next/link";
+
+export default function CategoryNav({category} :{category: Category}) {
+    return (
+        <>
+            <Link
+                key={category.id}
+                href={`/categories/${category.id}`}
+                className="text-white hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1">
+                {category.name}
+            </Link>
+        </>
+    )
+}
