01832de2d14d6a520876449fe10740d6b8318bdd
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-08 13:10:20 -0800

    is active added

--- components/UI/CategoryNav.tsx
+++ components/UI/CategoryNav.tsx
@@ -1,13 +1,17 @@
+'use client'
 import {Category} from "@/src/schema";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
 export default function CategoryNav({category} :{category: Category}) {
+    const pathname = usePathname();
+    const isActive = pathname === `/categories/${category.id}`;
     return (
         <>
             <Link
                 key={category.name+category.id}
                 href={`/categories/${category.id}`}
-                className="text-white hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1">
+                className={` hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1 ${isActive?'bg-fondo3 underline ': 'text-white'}`}>
                 {category.name}
             </Link>
         </>
