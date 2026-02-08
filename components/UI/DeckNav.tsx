781ef50540e70b34e92f4b3e5fbbee4dc6516841
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-08 13:11:47 -0800

    is active added

--- components/UI/DeckNav.tsx
+++ components/UI/DeckNav.tsx
@@ -1,13 +1,18 @@
+'use client'
 import {Deck } from "@/src/schema";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
+
 
 export default async function DeckNav({deck}: {deck: Deck}) {
+    const pathname = usePathname();
+    const isActive = pathname === `/decks/${deck.id}`;
 
     return (
         <Link
             key={deck.id}
             href={`/decks/${deck.id}`}
-            className="text-white hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1">
+            className={` hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1 ${isActive?'bg-fondo3 underline ': 'text-white'}`}>
             {deck.name}
         </Link>
     )
