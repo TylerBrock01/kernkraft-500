7eb611fbf77217a3c516f37e08f77ac1c3e5dbd4
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-08 12:30:01 -0800

    deck navigation added

--- components/UI/DeckNav.tsx
+++ components/UI/DeckNav.tsx
@@ -1,31 +1,14 @@
-import {redirect} from "next/navigation";
-import { DeckCategoryResponseSchema} from "@/src/schema";
+import {Deck, } from "@/src/schema";
 import Link from "next/link";
 
-async function getDecks(){
-    const url = `${process.env.API_URL}/decks`;
-    const req = await fetch(url, {
-        next:{
-            tags:['products-by-category']
-        }
-    })
-    const json = await req.json()
-    return DeckCategoryResponseSchema.parse(json)
-}
-export default async function DeckPage() {
-    const decksCategory = await getDecks()
+export default async function DeckPage({Deck}: {Deck: Deck}) {
 
     return (
-        <div className='flex flex-nowrap items-center justify-end '>
-            { decksCategory.map(deck =>
-                <Link
-                    key={deck.id}
-                    href={`/categories/${deck.id}`}
-                    className="text-white hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1">
-                    {deck.name}
-                </Link>
-            )
-            }
-        </div>
+        <Link
+            key={Deck.id}
+            href={`/categories/${Deck.id}`}
+            className="text-white hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1">
+            {Deck.name}
+        </Link>
     )
 }
