0b08d30450dd6839d3829e4bf9672f29d8f9f1aa
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-07 18:01:17 -0800

    link added

--- components/products/ProductCardDemo.tsx
+++ components/products/ProductCardDemo.tsx
@@ -1,11 +1,12 @@
 import {Deck} from "@/src/schema";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ProductCardDemo({deck}: { deck: Deck}) {
     return (
-        <div key={deck.id} className='flex flex-col bg-fondo2 justify-center rounded-md p-2 animate-zoom-in'>
+        <Link href={`/decks/${deck.id}`} key={deck.id} className='flex flex-col bg-fondo2 justify-center rounded-md p-2 animate-zoom-in'>
             <Image className={` rounded-md`} src={deck.image} alt={deck.name} width="100" height="100"/>
             <h2 className=" text-white underline " key={deck.id}>Go to {deck.name}</h2>
-        </div>
+        </Link>
     )
 }
