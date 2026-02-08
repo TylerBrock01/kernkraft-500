1dc4a2e6b806e1775d2e42877aed343b316a6009
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-08 12:20:13 -0800

    header added

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,6 +1,9 @@
 import MainNav from "@/components/UI/MainNav";
 import {ShoppingCart} from "@/components/cart/ShoppingCart";
 import ToastNotification from "@/components/UI/ToastNotification";
+import Logo from "@/components/UI/Logo";
+import Link from "next/link";
+import DeckPage from "@/components/UI/DeckNav";
 
 export default function RootLayout({
                                        children,
@@ -9,7 +12,10 @@ export default function RootLayout({
 }>) {
     return (
         <>
-            <MainNav/>
+            <header className=" px-5 md:px-10 py-1 gap-1 bg-fondo1 grid animate-slide-in-top">
+                <Logo/>
+                <MainNav/>
+            </header>
             <main className="lg:flex  lg:h-screen lg:overflow-y-hidden">
                 <div className="md:flex-1 md: md:overflow-y-scroll">
                     {children}
