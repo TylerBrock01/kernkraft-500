7474ac5cfcb0cc6609b5a66ea1ea98d0098c4044
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-08 12:50:36 -0800

    nav finished

--- components/UI/CategoryNav.tsx
+++ components/UI/CategoryNav.tsx
@@ -5,7 +5,7 @@ export default function CategoryNav({category} :{category: Category}) {
     return (
         <>
             <Link
-                key={category.id}
+                key={category.name+category.id}
                 href={`/categories/${category.id}`}
                 className="text-white hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1">
                 {category.name}
--- components/UI/DeckNav.tsx
+++ components/UI/DeckNav.tsx
@@ -1,14 +1,14 @@
-import {Deck, } from "@/src/schema";
+import {Deck } from "@/src/schema";
 import Link from "next/link";
 
-export default async function DeckPage({Deck}: {Deck: Deck}) {
+export default async function DeckNav({deck}: {deck: Deck}) {
 
     return (
         <Link
-            key={Deck.id}
-            href={`/categories/${Deck.id}`}
+            key={deck.id}
+            href={`/decks/${deck.id}`}
             className="text-white hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1">
-            {Deck.name}
+            {deck.name}
         </Link>
     )
 }
