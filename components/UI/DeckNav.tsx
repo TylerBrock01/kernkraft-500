b56b45393d86e895b4c623bbf52c1f02554ec52d
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-09 13:25:41 -0800

    async eliminated

--- components/UI/DeckNav.tsx
+++ components/UI/DeckNav.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 import {usePathname} from "next/navigation";
 
 
-export default async function DeckNav({deck}: {deck: Deck}) {
+export default function DeckNav({deck}: {deck: Deck}) {
     const pathname = usePathname();
     const isActive = pathname === `/decks/${deck.id}`;
 
     return (
         <Link
-            key={deck.id}
+            key={deck.name+deck.id}
             href={`/decks/${deck.id}`}
             className={` hover:text-black hover:underline hover:bg-fondo3 rounded-md  font-bold p-1 ${isActive?'bg-fondo3 underline ': 'text-white'}`}>
             {deck.name}
