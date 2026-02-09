adac4d9126b15400c7d3b6686404d193ccb32918
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-08 17:02:27 -0800

    linked

--- components/UI/Logo.tsx
+++ components/UI/Logo.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
     return (
-        <div className="grid grid-cols-1 md:flex md:justify-start md:items-end gap-1">
+        <Link href={'/'} className="grid grid-cols-1 md:flex md:justify-start md:items-end gap-1">
             <Image
                 src={"https://raw.githubusercontent.com/TylerBrock01/tienda/refs/heads/main/img/void.jpeg"}
                 alt={"vask8"}
@@ -14,7 +15,7 @@ export default function Logo() {
             <h1 className="text-3xl font-extrabold text-white">CRUX AERO ZENITH AGENCY {' '}
                 <span className="text-black underline under text-xl ">VASK8</span>
             </h1>
-        </div>
+        </Link>
 
     )
 }
