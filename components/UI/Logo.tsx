4c5906b8949e4bc6cc0bd0ab0d949b4d125c9e90
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-09 15:07:48 -0800

    Logo style updated

--- components/UI/Logo.tsx
+++ components/UI/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo() {
     return (
         <Link href={'/'} className="grid grid-cols-1 md:flex md:justify-start md:items-end gap-1">
-            <h1 className="text-3xl font-extrabold text-white">Skate Shop {' '}
+            <h1 className="text-3xl font-extrabold italic tracking-tighter text-white">Skate Shop {' '}
                 <span className="text-yellow-400 underline under text-xl ">VASK8</span>
             </h1>
         </Link>
