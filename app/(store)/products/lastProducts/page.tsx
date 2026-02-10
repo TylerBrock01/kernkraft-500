673a14b0d3ad873eb6701c050e105f364a3f1ce9
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-10 14:49:01 -0800

    able lastProducts

--- app/(store)/products/lastProducts/page.tsx
+++ app/(store)/products/lastProducts/page.tsx
@@ -3,21 +3,21 @@ import ProductCard from "@/components/products/ProductCards";
 import {redirect} from "next/navigation";
 import Link from "next/link";
 
-// async function getProduct() {
-//     const url = `${process.env.API_URL}/products?take=4`
-//     const req = await fetch(url,{
-//         next:{
-//             tags:['products-by-category']
-//         }
-//     })
-//     const json = await req.json()
-//     if (!req.ok){
-//         redirect('/categories/1')
-//     }
-//     return ProductResponseSchema.parse(json)
-// }
+async function getProduct() {
+    const url = `${process.env.API_URL}/products?take=4`
+    const req = await fetch(url,{
+        next:{
+            tags:['products-by-category']
+        }
+    })
+    const json = await req.json()
+    if (!req.ok){
+        redirect('/categories/1')
+    }
+    return ProductResponseSchema.parse(json)
+}
 export default async function LastProductPage() {
-    // const products = await getProduct()
+    const products = await getProduct()
     return(
         <section className="grid">
             <h2 className="italic text-4xl font-bold text-center text-white bg-black/50 py-2">
@@ -26,8 +26,8 @@ export default async function LastProductPage() {
             <div className="">
                 <div className={""}>
                     <div className='p-2 grid grid-cols-2 gap-2 md:grid-cols-4 xl:grid-cols-5 '>
-                        {/*{ products.products.map(product =>*/}
-                        {/*    <ProductCard key={product.id} product={product}/>)}*/}
+                        { products.products.map(product =>
+                            <ProductCard key={product.id} product={product}/>)}
                     </div>
                 </div>
             </div>
