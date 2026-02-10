9dfb74a0602f280d56c8fb4ed1fbaf87b7df7a04
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-10 00:03:36 -0800

    scroll added

--- components/UI/MainNav.tsx
+++ components/UI/MainNav.tsx
@@ -4,9 +4,14 @@ import DeckBar from "@/components/deck/DeckBar";
 export default async function MainNav() {
 
     return (
-        <div className="py-1 grid md:flex md:justify-between border-y border-y-amber-400/50">
-            <CategoryBar/>
-            <DeckBar/>
-        </div>
+        <>
+            <div className="overflow-x-auto py-1 grid md:flex md:justify-between border-t border-y-amber-400/50">
+                <CategoryBar/>
+            </div>
+            <div className="overflow-x-auto py-1 grid md:flex md:justify-between border-b border-y-amber-400/50">
+                <DeckBar/>
+            </div>
+        </>
+
     )
 }
