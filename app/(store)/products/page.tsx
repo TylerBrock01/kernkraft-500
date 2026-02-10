ad7a6a66706cde0961a5256bfbdac7c225ea7be6
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-10 15:53:39 -0800

    products page test

--- app/(store)/products/page.tsx
+++ app/(store)/products/page.tsx
@@ -2,9 +2,11 @@ import { ProductResponseSchema} from "@/src/schema";
 import ProductCard from "@/components/products/ProductCards";
 import {redirect} from "next/navigation";
 import Link from "next/link";
+import Pagination from "@/components/UI/Pagination";
+import {isValidPage} from "@/src/utils";
 
-async function getProduct() {
-    const url = `${process.env.API_URL}/products`
+async function getProducts(take:number, skip:number){
+    const url = `${process.env.API_URL}/products?take=${take}&skip=${skip}`;
     const req = await fetch(url,{
         next:{
             tags:['products-by-category']
@@ -14,23 +16,35 @@ async function getProduct() {
     if (!req.ok){
         redirect('/home')
     }
-    return ProductResponseSchema.parse(json)
+    const data = ProductResponseSchema.parse(json)
+    return {products: data.products, total: data.total};
 }
-export default async function ProductsPage() {
-    const products = await getProduct()
+type SearchParams = Promise<{page:string}>
+
+export default async function ProductsPage({searchParams}: {searchParams: SearchParams}) {
+    const {page} = await searchParams
+    console.log(page)
+    if (!isValidPage(+page)) redirect('/products?page=1');
+    const producstPerPage = 10
+    const skip = (+page - 1) * producstPerPage;
+    const {products, total} =await getProducts(producstPerPage,skip)
+    const totalPages = Math.ceil( total!/producstPerPage );
+    if (+page > totalPages) redirect('/products?page=1');
+
     return(
         <section className="grid">
             <h2 className="italic text-4xl font-bold text-center text-white bg-black/50 py-2">
                 Nuevos <span className="text-yellow-400"> Productos</span>
             </h2>
-            <div className="">
-                <div className={""}>
-                    <div className='p-2 grid grid-cols-2 gap-2 md:grid-cols-4 xl:grid-cols-5 '>
-                        { products.products.map(product =>
-                            <ProductCard key={product.id} product={product}/>)}
-                    </div>
-                </div>
+            <div className='p-2 grid grid-cols-2 gap-2 md:grid-cols-4 xl:grid-cols-5 '>
+                { products.map(product =>
+                    <ProductCard key={product.id} product={product}/>)}
             </div>
+            <Pagination
+                page={+page}
+                totalPages={+totalPages}
+                baseUrl={"/products"}
+            />
             <div className="text-center flex justify-center  p-3">
                 <Link href="#" className="   text-white hover:text-yellow-400 border border-white hover:border-yellow-400 py-3 px-8 rounded-full transition duration-300">
                     Ver Tienda Completa
