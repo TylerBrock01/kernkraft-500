53fc79df78bbf989dd91aae4103578ed6c54a7c9
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-11 09:41:29 -0800

    main nav display added

--- components/UI/MainNav.tsx
+++ components/UI/MainNav.tsx
@@ -4,14 +4,14 @@ import DeckBar from "@/components/deck/DeckBar";
 export default async function MainNav() {
 
     return (
-        <>
-            <div className="overflow-x-auto py-1 grid md:flex md:justify-between border-t border-y-amber-400/50">
+        <div className={"grid md:flex md:justify-between border-y border-y-amber-400/50"}>
+            <div className="overflow-x-auto py-1 grid md:flex md:justify-between ">
                 <CategoryBar/>
             </div>
-            <div className="overflow-x-auto py-1 grid md:flex md:justify-between border-b border-y-amber-400/50">
+            <div className="overflow-x-auto py-1 grid md:flex md:justify-between ">
                 <DeckBar/>
             </div>
-        </>
+        </div>
 
     )
 }
