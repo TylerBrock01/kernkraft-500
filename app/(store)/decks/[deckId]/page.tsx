7a0196de9f6f00429acaa5f1a7502a4bfa870d9d
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-11 20:25:49 -0800

    style changed

--- app/(store)/decks/[deckId]/page.tsx
+++ app/(store)/decks/[deckId]/page.tsx
@@ -29,11 +29,15 @@ export default async function StorePage({params}: { params: Params}) {
                 <h2 className={"text-zinc-200 mt-2 text-2xl p-1 animate-fade-in-right"}>{deck.name}:</h2>
 
             </div>
-            <div className='p-2 grid grid-cols-2 gap-2 md:grid-cols-4 xl:grid-cols-5 '>
-                { deck.products?.map(product =>
-                    <ProductCard key={product.id} product={product}/>
-                )
-                }
+            <div className="min-h-screen bg-black bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-zinc-900/20 via-black to-black py-5 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto">
+                    {/* Tu Heading aquí */}
+                    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-12">
+                        {deck.products.map(product => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     )
