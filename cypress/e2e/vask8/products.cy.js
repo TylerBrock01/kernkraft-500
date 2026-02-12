08b2e9c2ef78ec7fb89c7e63d2b0aa38878beb9c
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-12 11:49:09 -0800

    test added product

---
+++ cypress/e2e/vask8/products.cy.js
@@ -0,0 +1,17 @@
+describe('Pruebas de producto', () => {
+    beforeEach(() => {
+        cy.visit('/admin/products?page=1')
+    })
+    it('usar el form', () => {
+        cy.get('#add-new-product-link').click();
+        cy.get('#name').type('prueba');
+        cy.get('#price').type('100');
+        cy.get('#stock').type('10');
+        cy.get('#color').type('rojo')
+        cy.get('#size').type('8')
+        cy.get('#categoryId').select('1')
+        cy.get('#deckId').select('1')
+        cy.get('#submitProductButton').click()
+    });
+
+});
