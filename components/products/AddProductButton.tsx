aac40545e33475696faf0afd23141aca74a43c32
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-12 00:16:06 -0800

    toast added

--- components/products/AddProductButton.tsx
+++ components/products/AddProductButton.tsx
@@ -1,6 +1,7 @@
 "use client"
 import {Product} from "@/src/schema";
 import {useStore} from "@/src/store";
+import {toast} from "react-toastify";
 
 export default function AddProductButton({product}: { product: Product}) {
 
@@ -9,7 +10,10 @@ export default function AddProductButton({product}: { product: Product}) {
         <>
             <button
                 type="button"
-                onClick={()=> addToCart(product)}
+                onClick={()=> {
+                    addToCart(product)
+                    toast.success('Producto agregado al carrito')
+                }}
                 className="h-11 w-11 flex items-center justify-center rounded-xl bg-white text-black transition-all duration-300 hover:bg-yellow-400 hover:scale-110 active:scale-95 shadow-lg">
                 <svg xmlns="http://www.w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
                     <line x1="12" y1="5" x2="12" y2="19"></line>
