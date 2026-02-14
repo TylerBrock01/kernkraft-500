f2712eab09f5447e897c04b25153737756b91b33
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-14 13:00:48 -0800

    table changed responsive

--- components/products/ProductsTable.tsx
+++ components/products/ProductsTable.tsx
@@ -7,10 +7,10 @@ import DeleteProduct from "@/components/products/DeleteProductForm";
 export default function ProductsTable({products} : {products: Product[]}) {
 
     return (
-        <div className="px-4 sm:px-6 lg:px-8 mt-10">
+        <div className="md:px-4 sm:px-6 lg:px-8 md:mt-10">
             <div className="mt-8 flow-root ">
                 <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 bg-white p-5 ">
+                    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 bg-white md:p-5 ">
                         <table className="min-w-full divide-y divide-gray-300 ">
                             <thead>
                             <tr>
