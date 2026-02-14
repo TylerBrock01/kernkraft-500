cc25e81d89e25b88a314127906493f2756b7f009
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-14 12:45:57 -0800

    content sm updated children

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({children,}: Readonly<{
     return (
         <>
             <AdminNav/>
-            <div className="lg:min-h-screen container mx-auto mt-10 px-10 lg:px-0">
+            <div className="lg:min-h-screen container mx-auto mt-10 px-4 md:px-10 lg:px-0">
                 <div className="bg-white shadow w-full  mx-auto p-10 my-10 lg:w-3/5" >
                     {children}
                 </div>
