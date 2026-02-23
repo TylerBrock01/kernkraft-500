5694cba06e3865fe79df75570fcc047ed7ec4ea4
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-02-22 17:21:52 -0800

    redirect added

--- actions/submit-order-action.ts
+++ actions/submit-order-action.ts
@@ -10,9 +10,9 @@ export async function submitOrderAction(data: unknown) {
     const token = cookieStore.get('skate_token')?.value;
 
     // 2. Si no hay token, mandamos al login (Esto detiene la ejecución aquí)
-    // if (!token) {
-    //     redirect('/auth/login');
-    // }
+    if (!token) {
+        redirect('/auth/login');
+    }
 
     // 3. Validar datos con Zod
     const order = OrderSchema.parse(data);
@@ -39,7 +39,7 @@ export async function submitOrderAction(data: unknown) {
 
         const parsedError = ErrorResponseSchema.safeParse(json);
         return {
-            errors: parsedError.success ? parsedError.data.message : ["Error inesperado en el servidor"],
+            errors: parsedError.success ? parsedError.data.message : ["Inicia sesion"],
             success: ""
         };
     }
