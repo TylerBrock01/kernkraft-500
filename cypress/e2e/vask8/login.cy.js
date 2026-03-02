e7cba24397b36ed5493ad7ad598c5aaa02484156
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-03-02 14:58:54 -0800

    transaction admin

--- cypress/e2e/vask8/login.cy.js
+++ cypress/e2e/vask8/login.cy.js
@@ -7,12 +7,13 @@ describe('Login', () => {
         "email": "test1@test1.com",
         "password": "vask177",
     }
+    const coupon = 'VASK8_PRO_2026'
 
     beforeEach(() => {
         cy.visit('/auth/login')
         cy.clearCookies('skate_token')
     })
-    it.only('should login', () => {
+    it('should login', () => {
         cy.get('#email').type(admin.email)
         cy.get('#password').type(admin.password)
         cy.get('#logInButton').click()
@@ -24,17 +25,25 @@ describe('Login', () => {
         cy.get('#logoutButton').click()
     })
 
-    it('should create transaction as admin', () => {
+    it.only('should create transaction as admin w/coupon', () => {
         cy.get('#email').type(admin.email)
         cy.get('#password').type(admin.password)
         cy.get('#logInButton').click()
 
         cy.get('#store').click()
-        cy.wait(1500)
+        cy.wait(500)
         cy.visit('/products?page=1')
         cy.get('#product-card-1').click()
         cy.get('#add-product-button-1').click();
+        cy.get('#cart-button').click()
+
+        cy.get('#coupon-input').type(coupon)
+        cy.get('#coupon-submit').click()
+
         // cy.get('#submit-order-button').click()
+
+        cy.get('#profile-button').click()
+
     })
     it('should see transactions as admin ', () => {
         cy.get('#email').type(admin.email)
