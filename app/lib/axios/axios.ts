bb029c2e5f277d872eedbedfe1cde42466356b65
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-04-03 22:47:10 -0700

    axios added

---
+++ app/lib/axios/axios.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+// Apuntamos al motor de NestJS
+const API_URL = 'http://localhost:3000';
+
+export const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// Interceptor: Antes de que cualquier petición salga, le pegamos el JWT
+api.interceptors.request.use((config:any) => {
+    const token = Cookies.get('caza_token'); // Buscamos la cookie
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
