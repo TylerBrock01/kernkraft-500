7f6d39d6712e8f9b4bffa3c69c782b36dd167389
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-04-04 23:35:47 -0700

    demo fix v2: updated API_URL

--- app/lib/axios/axios.ts
+++ app/lib/axios/axios.ts
@@ -1,19 +1,21 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-// Apuntamos al motor de NestJS
-// const API_URL = 'http://localhost:3000';
-const API_URL = process.env.API_URL;
+// 🚀 REGLA DE ORO: Las variables de frontend DEBEN empezar con NEXT_PUBLIC_
+// Le ponemos el fallback a localhost para que no tengas que cambiar el código al probar en tu PC
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
 
 export const api = axios.create({
     baseURL: API_URL,
+    // ⚠️ Agregamos 60 segundos de paciencia para darle tiempo a Render de despertar del modo gratuito
+    timeout: 60000,
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
 // Interceptor: Antes de que cualquier petición salga, le pegamos el JWT
-api.interceptors.request.use((config:any) => {
+api.interceptors.request.use((config: any) => {
     const token = Cookies.get('caza_token'); // Buscamos la cookie
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
