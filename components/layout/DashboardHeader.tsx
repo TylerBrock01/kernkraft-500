b34952fbb9a3851dc6443245423d3bc4d324cf0a
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2026-05-26 20:49:17 -0700

    revert header deletion

--- components/layout/DashboardHeader.tsx
+++ components/layout/DashboardHeader.tsx
@@ -7,12 +7,12 @@ interface DashboardHeaderProps {
 
 export default function DashboardHeader({ user, onOpenSidebar }: DashboardHeaderProps) {
     return (
-        <header className=" border-b border-zinc-900 flex items-center justify-between px-4 md:px-8 bg-zinc-950/80 backdrop-blur-md z-30 shrink-0">
+        <header className="lg:hidden h-16 border-b border-zinc-900 flex items-center justify-between px-4 md:px-8 bg-zinc-950/80 backdrop-blur-md z-30 shrink-0">
             <div className="flex items-center gap-4">
                 {/* 📱 Botón de Hamburguesa (Visible en Móvil y Tablet, oculto en PC 'lg:hidden') */}
                 <button
                     onClick={onOpenSidebar}
-                    className="lg:hidden p-2 -ml-2 text-zinc-400 hover:text-white transition-colors"
+                    className="p-2 -ml-2 text-zinc-400 hover:text-white transition-colors"
                 >
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
                 </button>
